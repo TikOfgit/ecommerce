85dfadc123ed0467970165a7bdf121b3f7495c36
TikOfgit <tikofmine@gmail.com>
2024-12-10 18:55:13 +0100

    Add i.ibb.co to allowed image domains in Next.js config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['your-supabase-project.supabase.co'],
+    domains: ['your-supabase-project.supabase.co', 'i.ibb.co'],
   },
 }
 
